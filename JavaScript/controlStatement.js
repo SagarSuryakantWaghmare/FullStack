1c156ac9671e3ba695d906f6254e06c070319d46
Sagar <sagarwaghmare1384@gmail.com>
2024-12-07 05:52:39 +0530

    Control Statement

--- JavaScript/controlStatement.js
+++ JavaScript/controlStatement.js
@@ -1,17 +1,35 @@
-var i=1;
-while(i<2){
-    console.log(i);
-    i++;
+// var i=1;
+// while(i<2){
+//     console.log(i);
+//     i++;
+// }
+// var numberOfBottles = 99
+// while (numberOfBottles >= 0) {
+//     var bottleWord = "bottle";
+//     if (numberOfBottles === 1) {
+//         bottleWord = "bottles";
+//     } 
+//     console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+//     console.log(numberOfBottles + " " + bottleWord + " of beer,");
+//     console.log("Take one down, pass it around,");
+// 	numberOfBottles--;
+//     console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+// }
+
+function fibonacciGenerate(n){
+var output=[];
+if(n==1){
+    output=[0];
 }
-var numberOfBottles = 99
-while (numberOfBottles >= 0) {
-    var bottleWord = "bottle";
-    if (numberOfBottles === 1) {
-        bottleWord = "bottles";
-    } 
-    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
-    console.log(numberOfBottles + " " + bottleWord + " of beer,");
-    console.log("Take one down, pass it around,");
-	numberOfBottles--;
-    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+else if (n==2){
+    output=[0,1];
 }
+else{
+    output=[0,1];
+    for(var i=2;i<n;i++){
+       output.push( output[output.length-2]+output[output.length-1] );
+   }
+}
+return output;
+}
+console.log(fibonacciGenerate(4))
