95bc893c221e0815f44500547de442fa4a2f9d02
Sagar <sagarwaghmare1384@gmail.com>
2024-12-07 23:28:59 +0530

    Npm packages learned

--- BackendDevelopement/node/index.js
+++ BackendDevelopement/node/index.js
@@ -1,9 +1,17 @@
 // const fs=require('fs');
-// // fs.writeFile("message.txt","Hello Shraddha",(err)=>{
-// //     if(err) throw err;
-// //     console.log("The file has been saved")
-// // })
+// fs.writeFile("message.txt","Hello Shraddha",(err)=>{
+//     if(err) throw err;
+//     console.log("The file has been saved")
+// })
 // fs.readFile("./message.txt",'utf-8',(err,data)=>{
 //     if(err) throw err;
 //     console.log(data)
 // })
+
+// var generateName=require("sillyname");
+// var sillyname=generateName();
+// console.log(sillyname)
+
+import heroname from "superheroes"
+const name=heroname.random();
+console.log(`I am ${name}`);
