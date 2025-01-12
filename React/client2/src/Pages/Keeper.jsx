78fcfec38168f0891b5b6de244b3e200c4d0ac7c
Sagar <sagarwaghmare1384@gmail.com>
2025-01-12 13:29:38 +0530

    Added the keeper

--- React/client2/src/Pages/Keeper.jsx
+++ React/client2/src/Pages/Keeper.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
-
+import AddIcon from "@mui/icons-material/Add";
+import { Fab } from "@mui/material";
+import { Zoom } from "@mui/material";
+import HighlightIcon from "@mui/icons-material/Highlight";
+import DeleteIcon from "@mui/icons-material/Delete";
 function Keeper() {
   const [task, setTask] = useState({
     title: "",
     task: ""
   });
   const [tasks, setTasks] = useState([]);
+  const [show, setShow] = useState(false);
   const handleChange = (e) => {
+    if ( e.target.value.length > 0) {
+      setShow(true);
+    }
     const { name, value } = e.target;
     setTask((prevState) => ({
       ...prevState,
@@ -21,17 +29,19 @@ function Keeper() {
     });
   };
   const handleDelete = (index) => {
-    return()=>{
-        const newTask=tasks.filter((task,i)=>i!==index);
-        setTasks(newTask);
+    return () => {
+      const newTask = tasks.filter((task, i) => i !== index);
+      setTasks(newTask);
     }
-}
+  }
   return (
     <>
       <div style={{ display: "flex", justifyContent: "center", backgroundColor: "orange", color: "white", height: "50px", alignItems: "center" }}>
-        <h1>Todo List</h1>
+        <h1><HighlightIcon/>Todo List</h1>
       </div>
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "20px" }}>
+    
+      <div
+        style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "20px" }}>
         <input
           onChange={handleChange}
           value={task.title}
@@ -47,9 +57,13 @@ function Keeper() {
           name="task"
           style={{ marginBottom: "10px", padding: "10px", width: "300px", height: "100px" }}
         />
-        <button onClick={handleSubmit} style={{ padding: "10px 20px", backgroundColor: "blue", color: "white", border: "none", cursor: "pointer" }}>
-          Add
-        </button>
+        {
+          show && <Zoom in={true}>
+            <Fab className="AddIcon" onClick={handleSubmit} style={{ padding: "10px 20px", backgroundColor: "blue", color: "white", border: "none", cursor: "pointer" }}>
+              <AddIcon />
+            </Fab>
+          </Zoom>
+        }
       </div>
       <div>
         <h1>Your Tasks</h1>
@@ -72,7 +86,7 @@ function Keeper() {
               >
                 <h3>{task.title}</h3>
                 <p>{task.task}</p>
-                <button key={index} onClick={handleDelete(index)}>Delete</button>
+                <Fab key={index} style={{ backgroundColor: "white" }} onClick={handleDelete(index)}><DeleteIcon /></Fab>
               </li>
             ))}
           </ul>
