b41f0f8a3b8f7862a6c14005998cc3371995818c
Sagar <sagarwaghmare1384@gmail.com>
2025-01-17 14:09:50 +0530

    learning of the postgress

---
+++ PostGSql/WorldCaptialQuiz/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // ...existing code...
+  database: {
+    host: 'localhost',
+    user: 'postgres',
+    password: 'your_correct_password',
+    database: 'your_database_name',
+    port: 5432
+  }
+  // ...existing code...
+};
